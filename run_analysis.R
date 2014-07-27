7ccfa0c8a8c3424edf783176db6a7f83db12c0dc
CoolCoolCoolCool <collinberickson@gmail.com>
2014-07-27 14:35:40 -0500

    Added comments to run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,35 @@
+# Stores activities names for later use
+activities <- c('WALKING','WALKING_UPSTAIRS','WALKING_DOWNSTAIRS','SITTING','STANDING','LAYING')
 
-
-merge.temp <- function(testortrain) {
-  subt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','subject_',testortrain,'.txt',sep=''),col.names='subject')
-  xt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','X_',testortrain,'.txt',sep=''))
-  xt <- xt[c(col.which.mean,col.which.std)]
-  names(xt) <- c(col.names.mean,col.names.std)
-  yt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','Y_',testortrain,'.txt',sep=''),col.names = c('Y'))
-  cbind(subt,xt,yt)
-}
-
-train <- merge.temp('train')
-test <- merge.temp('test')
-
-d <- rbind(train,test)
-
+# Prepare to only use columns we want: those with mean and standard deviations
 col.names <- read.table('./UCI HAR Dataset/features.txt')
 col.which.mean <- grep('mean',col.names$V2,fixed=T)
 col.names.mean <- as.character(col.names$V2[col.which.mean])
 col.which.std <- grep('std',col.names$V2,fixed=T)
 col.names.std <- as.character(col.names$V2[col.which.std])
-cbind(colMeans(test),apply(test,2,sd))
+
+# A function to load the data
+merge.temp <- function(testortrain) {
+  subt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','subject_',testortrain,'.txt',sep=''),col.names='subject')
+  xt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','X_',testortrain,'.txt',sep=''))
+  xt <- xt[c(col.which.mean,col.which.std)]
+  names(xt) <- c(col.names.mean,col.names.std)
+  yt <- read.table(paste('./UCI HAR Dataset/',testortrain,'/','Y_',testortrain,'.txt',sep=''),col.names = c('activity'))
+  cbind(subt,yt,xt)
+}
+
+# Loads the data
+train <- merge.temp('train')
+test <- merge.temp('test')
+
+# Combines train and test into one data frame
+merged.data <- rbind(train,test)
+
+# Reduce size of data
+tidy.data <- ddply(merged.data,c('subject','activity'),colMeans)
+
+# Change column with activity from number to word description 
+tidy.data$activity <- activities[tidy.data$activity]
+
+# Write to file
+write.table(tidy.data,file='output.txt')
